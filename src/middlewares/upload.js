336a0a42b33337c0ca656e56913f03a0be81b90f
Yones Maheri <yonesmaheri80@gmail.com>
2025-08-21 19:58:27 +0330

    testing bug fix 2

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "..", "uploads"));
+    cb(null, "/app/uploads");  
   },
   filename: function (req, file, cb) {
     const ext = path.extname(file.originalname);
