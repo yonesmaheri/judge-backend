2a19353263a9e2f3870154b7b20582a6f0fb1954
Yones Maheri <yonesmaheri80@gmail.com>
2025-08-31 10:14:46 +0330

    get ready for local test

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -32,11 +32,8 @@ async function login(req, res) {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: true,
       sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      domain: ".yonesma.ir",
-      path: "/",
     });
 
     return res.json({ message: "Logged in successfuly" });
--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -24,7 +24,6 @@ async function register(req, res) {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: true,
     sameSite: "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
