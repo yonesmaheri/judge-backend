a212952d1611a069f875ca1b2d6306b15a23704b
Yones Maheri <yonesmaheri80@gmail.com>
2025-09-01 11:04:42 +0330

    logs for cors on server

--- src/app.js
+++ src/app.js
@@ -18,13 +18,18 @@ const allowedOrigins = [
 
 const corsOptions = {
   origin: function (origin, callback) {
+    console.log("🔎 Origin دریافت شده:", origin);
+
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
+      console.log("❌ Origin غیرمجاز:", origin);
       callback(new Error("Not allowed by CORS"));
     }
   },
   credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 app.use(cors(corsOptions));
